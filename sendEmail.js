ad25c460fe629c5bc18e8adb60fd2641804e589b
deepu105 <d4udts@gmail.com>
2020-11-12 15:58:01 +0100

    polish

--- sendEmail.js
+++ sendEmail.js
@@ -22,7 +22,7 @@ const csvFile = params[2];
 const fromEmail = process.env.EMAIL_FROM;
 const currentPath = process.cwd();
 
-const timeoutMs = 1000; // Timeout used for spreading out load
+const timeoutMs = 5000; // Timeout used for spreading out load
 
 if (csvFile) {
   console.log("CSV File", csvFile);
@@ -53,7 +53,6 @@ const sendEmail = (toEmailId, html, subject, close) =>
 
     smtpTransport.sendMail(msg, (err) => {
       if (err) {
-        console.error("Sending to " + toEmailId + " failed: " + err);
         return reject(err);
       }
       if (close) {
@@ -67,41 +66,49 @@ const csvFilePath = currentPath + "/" + csvFile;
 
 console.log("csvFilePath", csvFilePath);
 
-const getMsgParams = (it) => {
+const getMsgParams = (name) => {
   const signature = `
     Deepu K Sasidharan<br>
     <i>Developer Advocate</i><br><br>
+    <i>Mobile: +31 657 635 007</i><br>
     <i>Twitter: @deepu105</i><br>
-    <a href="https://www.adyen.com/" display: inline-block;"><img src="https://i.imgur.com/KB3fWTZ.png" width="125" height="42" style="width: 125px; height: 42px; border: none;"></a>
+    <a href="https://www.adyen.com/" style="color: rgb(27, 106, 203); display: inline-block;"><img src="https://i.imgur.com/KB3fWTZ.png" width="125" height="42" style="width: 125px; height: 42px; border: none;"></a>
     `;
   const senderName = "Deepu K Sasidharan";
-  const subject = `Hi ${it.name} - You are invited to our online session`;
-  const emailId = it.email;
+  const subject = `Hi ${name} - You are invited to our online session`;
   const htmlBody = `
-      Hi ${it.name},<br/><br/>
+      Hi ${name},<br/><br/>
 
-      Hope you’re doing well. My name is ${senderName} and I am a developer advocate at <a href="https://www.adyen.com/">Adyen</a>.<br/><br/>
+      Hope you’re doing well. My name is ${senderName} and I am a developer advocate at <a href="https://www.adyen.com/">Adyen</a>. I wanted to invite you to a live stream session. Normally, we host meetups, but given the uncertain times, we’re moving this edition online.<br/><br/>
 
+      The session begins on <strong>Wednesday, November 25th, at 17:00 CET</strong>. We’ll be building a Java 11 web application using Spring Boot and integrating the Adyen Drop-in component to enable online checkouts. We will cover Adyen Java Library integration, Adyen web component integration, and so on. We will also answer any questions that you have.<br/><br/>
+
+      We’d love for you to join and share your thoughts and suggestions. We’ll be streaming live in about two weeks, so let me know if you will be attending or reserve your virtual seat directly on the <a href="https://www.adyen.com/landing/education-sub-pages/global/developer-tutorials">session signup page</a> so that you can receive updates and reminders. Feel free to let me know if you have any questions or if you don’t want to receive any such emails from me in the future.<br/><br/>
+      
+      Hope to see you there!<br/><br/><br/>
+      
       Best,<br/>
 
       ${signature}
     `;
 
-  return [subject, emailId, htmlBody];
+  return [subject, htmlBody];
 };
 
 function sendBulkEmail() {
   csv()
     .fromFile(csvFilePath)
     .then((jsonObj) => {
-      jsonObj.map(async (i) => {
-        const [subject, emailId, htmlBody] = getMsgParams(i);
+      jsonObj.map(async (it) => {
+        const { name, email } = it;
+        const [subject, htmlBody] = getMsgParams(name);
 
-        console.log("Sending to emailId:", emailId, ", subject:", subject);
+        console.log(`Sending to emailId: ${email} subject: ${subject}`);
         try {
-          await sendEmail(emailId, htmlBody, subject);
-          console.log("success:sent:::==", i.name);
+          await sendEmail(email, htmlBody, subject);
+          console.log("success:sent:::==", name);
         } catch (e) {
+          console.error(`Sending to "${name},${email}" failed: ${e}`);
           console.log("error", e);
         }
       });
